f63d05bc5925e2f61b6caed41920f4f8e351a6a6
TRTX-gamer <77086233+TRTX-gamer@users.noreply.github.com>
2021-02-28 21:14:23 +0800

    better

--- test.cpp
+++ test.cpp
@@ -1,14 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    int a[ ][3] = { {1,4}, {3,2}, {4,5,6}, {0} };
-    printf("%d\n", a[0][0]);
-    printf("%d\n", a[0][1]);
-    printf("%d\n", a[0][2]);
-    printf("%d\n", a[1][0]);
-    printf("%d\n", a[1][1]);
-    printf("%d\n", a[1][2]);
-    printf("%d\n", a[2][0]);
-    printf("%d\n", a[2][1]);
-    printf("%d\n", a[2][2]);
+    char a4[15]={"Hello World"};
+    printf("%\n",a4[14]);
 }
