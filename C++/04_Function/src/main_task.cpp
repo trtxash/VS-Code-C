bf76e0a35f2002e5e441e41122d28e52c4abf0e8
w1165577077@qq.com <w1165577077@qq.com>
2025-03-07 01:42:26 +0800

    feat: 添加任务2和任务3功能及优化输入处理

--- C++/04_Function/src/main_task.cpp
+++ C++/04_Function/src/main_task.cpp
@@ -3,16 +3,30 @@
  * @brief   C++ Primer Plus Ch.7~ch.8
  * @author 	TRTX-gamer
  * @version 0.0.1
- * @date 	2025年3月5号1点42分
+ * @date 	2025年3月7号1点42分
  */
 
 #include <iostream>
-#include <limits>
 #include <vector>
+#include <cctype>
 
 using namespace std;
 
-// vecotr
+// cctype功能
+// 判断字符是否属于特定类别（如字母、数字等）。
+// isalpha(c) // 是否为字母 (a-z, A-Z)
+// isdigit(c) // 是否为数字 (0-9)
+// isalnum(c) // 是否为字母或数字
+// isspace(c) // 是否为空白字符（空格、换行符、制表符等）
+// islower(c) // 是否小写字母
+// isupper(c) // 是否大写字母
+// tolower(c)  // 转小写（若原为大写）
+// toupper(c)  // 转大写（若原为小写）
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// vecotr功能
+// 应避免长期持有vector元素的引用，或在可能改变容量的操作后及时更新引用。
+// 应避免长期持有vector元素的引用，或在可能改变容量的操作后及时更新引用。
+// 应避免长期持有vector元素的引用，或在可能改变容量的操作后及时更新引用。
 // vector<int> vec;                    // 创建一个空的 int 类型 vector
 // vector<int> vec2(10);               // 创建一个包含 10 个元素的 vector，默认值为 0
 // vector<int> vec3(10, 5);            // 创建一个包含 10 个元素的 vector，每个元素初始化为 5
@@ -61,8 +75,21 @@ struct DataVectorStruct
     vector<DataUnion> vec_union;
 };
 
+struct CandyBar // task2
+{
+    string brand;
+    float weight;
+    int calories;
+};
+
+CandyBar candy1 = {}; // task2
+
 template <typename AnyType>
-bool DateInput(AnyType &value, AnyType min, AnyType max);
+bool NumberInput(AnyType &value, AnyType min, AnyType max);
+
+void task1(string *s, int mode);
+void task2(CandyBar &candy, const char *ch = "Millenium Munch", const double d = 2.85, const int i = 350);
+void task3(string &s);
 
 /**
  * @brief	对函数简要描述
@@ -75,34 +102,83 @@ int main()
 {
     DataVectorStruct data_vec = {};
 
+    data_vec.vec_int.reserve(10); // 预分配足够空间，避免频繁重新分配内存，确保引用不会因vector扩容失效。
+    data_vec.vec_string.reserve(10);
+
     data_vec.vec_int.push_back(0);
-    int &task_num = data_vec.vec_int.at(0); // 重命名为任务变量，来自vector结构体的int临时变量
+    int &task_num = data_vec.vec_int.back(); // 重命名为任务变量，来自vector结构体的int临时变量
 
     while (1)
     {
+        // int mode_num = 0;
         cout << "Enter task number: ";
-        if (DateInput(task_num, 1, 9))
+        if (NumberInput(task_num, 1, 9))
         {
-            switch (task_num) // switch语句里不能定义变量，因分支共用变量
+            switch (task_num) // switch语句里不能定义变量，因分支共用变量,故定义处加大括号，定义在块内
             {
             case 1:
-                break;
+            {
+                cout << "Enter mode number: ";
+                data_vec.vec_int.push_back(0);
+                int &mode_num = data_vec.vec_int.back();
+                while (!NumberInput(mode_num, 0, 0))
+                    continue; // 输入错误，重新输入
+
+                data_vec.vec_string.push_back("Hello, world!");
+                string *s = &data_vec.vec_string.back();
+                task1(s, mode_num); // 调用task1函数，打印字符串s多次
+
+                data_vec.vec_int.pop_back(); // 删除临时变量
+                data_vec.vec_string.pop_back();
+            }
+            break;
             case 2:
-                break;
+            {
+                task2(candy1);
+            }
+            break;
             case 3:
-                break;
+            {
+                string s = {};
+                while (1)
+                {
+                    cout << "Enter a string: (q to quit)";
+                    getline(cin, s);
+                    if ((s[0] == 'q' || s[0] == 'Q') && s.size() == 1) // string直接就是“q”或“Q”
+                    {
+                        cout << "Bye!" << endl;
+                        break;
+                    }
+
+                    task3(s);
+                    cout << s << endl;
+                }
+            }
+            break;
             case 4:
-                break;
+            {
+            }
+            break;
             case 5:
-                break;
+            {
+            }
+            break;
             case 6:
-                break;
+            {
+            }
+            break;
             case 7:
-                break;
+            {
+            }
+            break;
             case 8:
-                break;
+            {
+            }
+            break;
             case 9:
-                break;
+            {
+            }
+            break;
             default:
                 cout << "no task : " << task_num << endl;
                 break;
@@ -113,40 +189,118 @@ int main()
         }
     }
 
+    data_vec.vec_int.pop_back();
     return 0;
+}
+
+/**
+ * @brief	任务3的实现
+ * @param
+ * @arg
+ * @note    默认参数写在函数声明处
+ * @retval
+ */
+void task3(string &s)
+{
+    for (int i = 0; i < s.length(); i++) // sizeof(string)在编译期确定，与字符串内容无关；length()在运行时计算，随字符串长度变化
+    {
+        if (isalpha(s[i]))
+        {
+            if (islower(s[i])) // 小写就转大写
+            {
+                s[i] = toupper(s[i]);
+            }
+        }
+    }
+}
+
+/**
+ * @brief	任务2的实现
+ * @param
+ * @arg
+ * @note    默认参数写在函数声明处
+ * @retval
+ */
+void task2(CandyBar &candy, const char *ch, const double d, const int i)
+{
+    candy.brand = ch;
+    candy.weight = d;
+    candy.calories = i;
+    cout << "Brand: " << candy.brand << endl;
+    cout << "Weight: " << candy.weight << endl;
+    cout << "Calories: " << candy.calories << endl;
+}
+
+/**
+ * @brief	任务1的实现
+ * @param 	s 	    字符串指针
+ * @param 	mode 	模式，非零则字符串打印次数为此函数调用次数，否则打印次数为1
+ * @arg
+ * @note
+ * @retval
+ */
+void task1(string *s, int mode)
+{
+    static int count = 0;
+    count++;
+    if (count >= 2147483647)
+    {
+        count = 0;
+    }
+    if (mode)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            cout << *s << " (" << count << " times)" << endl;
+        }
+    }
+    else
+    {
+        cout << *s << " (" << count << " times)" << endl;
+    }
 }
 
 /**
  * @brief	模板函数，读入输入的数据，并进行类型和范围检查，返回是否成功
  * @param 	value 	输入数据
  * @param 	min 	最小值
- * @param 	max 	最大值
+ * @param 	max 	最大值,都为0则不进行范围检查
  * @arg
  * @note  	只能处理整数、浮点数、布尔型数据类型，其他类型需要自行定义模板函数；
  * @retval	True    成功
  * @retval	False   失败
  */
 template <typename AnyType>
-bool DateInput(AnyType &value, AnyType min, AnyType max)
+bool NumberInput(AnyType &value, AnyType min, AnyType max)
 {
     if (!(cin >> value))
     {
         // 处理类型错误...
         cout << "Invalid task number format. Please enter a valid task number." << endl;
-        cin.clear();                                         // 必须先清除错误状态
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // 表示流的最大可能长度（通常是2^63-1）彻底地清除输入缓冲区的残留内容,不同系统的输入缓冲区最大值可能不同，使用标准库方法无需硬编码数值}
+        cin.clear();              // 必须先清除错误状态
+        while (cin.get() != '\n') // 删除没有用的输入
+            continue;             // get rid of bad input
         return false;
     }
-    if (value < min || value > max)
+    if (min == 0 && max == 0) // 不进行范围检查
     {
-        // 处理范围错误...
-        cout << "Invalid task number range. Please enter a valid task number." << endl;
-        cin.clear();                                         // 必须先清除错误状态
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // 表示流的最大可能长度（通常是2^63-1）彻底地清除输入缓冲区的残留内容,不同系统的输入缓冲区最大值可能不同，使用标准库方法无需硬编码数值}
-        return false;
     }
-    // 读取成功，清空缓冲区,防止getline()读取到换行符
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // 表示流的最大可能长度（通常是2^63-1）彻底地清除输入缓冲区的残留内容,不同系统的输入缓冲区最大值可能不同，使用标准库方法无需硬编码数值}
+    else
+    {
+        if (value < min || value > max)
+        {
+            // 处理范围错误...
+            cout << "Invalid task number range. Please enter a valid task number." << endl;
+            cin.clear();              // 必须先清除错误状态
+            while (cin.get() != '\n') // 删除没有用的输入
+                continue;             // get rid of bad input
+            return false;
+        }
+    }
+    while (cin.get() != '\n') // 删除没有用的输入
+        continue;             // get rid of bad input
+    // // 读取成功，清空缓冲区,防止getline()读取到换行符
+    // cin.ignore(numeric_limits<streamsize>::max(), '\n'); // 表示流的最大可能长度（通常是2^63-1）彻底地清除输入缓冲区的残留内容,不同系统的输入缓冲区最大值可能不同，使用标准库方法无需硬编码数值}
 
     return true;
 }
