07e60ecc4798e1697ea7a2c991028b80cc2ab38e
“trtxash” <w1165577077@qq.com>
2025-05-19 13:16:00 +0800

    new

---
+++ C_C++/01_Data_structure_Ch1/src/main.c
@@ -0,0 +1,26 @@
+/**
+ * @file	Data structure ch1
+ * @brief   
+ * @author 	trtxash
+ * @version 0.0.1
+ * @date 	2025-05-19 13:11
+ */
+#include <stdio.h>
+
+/**
+ * @brief	对函数简要描述
+ * @param 	参数说明，以’:’作为参数结束标志；
+ * @arg
+ * 参数里面可选择参量列举，对于可数情况可进行参量列举，同样以’:’作为参数结束标志；
+ * @note  	注释，配合brief一起使用可以达到很好的注释效果；
+ * @retval	返回值说明。
+ */
+int main()
+{
+    printf("Hello, World!\n");
+    while (1)
+    {
+    }
+
+    return 0;
+}
