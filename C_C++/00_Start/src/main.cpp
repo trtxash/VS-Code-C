e6f982d06b397d8cc2c4ddc6bfe573a238f6f193
“trtxash” <w1165577077@qq.com>
2025-05-19 13:03:55 +0800

    refactor: 更新编译设置并简化主程序

---
+++ C_C++/00_Start/src/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file	Basic C++ Program
+ * @brief   C++ Primer Plus Ch.12~ch.13
+ * @author 	TRTX-gamer
+ * @version 0.0.1
+ * @date 	2025-03-16 18:43
+ */
+#include <iostream>
+
+/**
+ * @brief	对函数简要描述
+ * @param 	参数说明，以’:’作为参数结束标志；
+ * @arg
+ * 参数里面可选择参量列举，对于可数情况可进行参量列举，同样以’:’作为参数结束标志；
+ * @note  	注释，配合brief一起使用可以达到很好的注释效果；
+ * @retval	返回值说明。
+ */
+int main()
+{
+    while (1)
+    {
+    }
+
+    return 0;
+}
